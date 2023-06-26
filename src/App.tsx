9fb135a2ce80a89f273bdd23a99ab8129d733214
Adam Amatavivadhana <adam.amata@yahoo.com>
2023-06-26 13:46:46 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [amountOfItems, setAmountOfItems] = useState(0);
+  const [checkoutLines, setCheckoutLines] = useState([[10, 5, 3], [], [], [], []]);
+
+  function handleItemSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        <form className='inputs' onSubmit={handleItemSubmit}>
+          <input
+            placeholder='Amount of Items'
+            type='number'
+            value={amountOfItems}
+            onChange={(e) => {setAmountOfItems(Number(e.target.value))}}
+          />
+          <button>Checkout</button>
+        </form>
+
+        <div className='lines'>
+          {checkoutLines.map((e) => ( 
+            <div className='line'>line</div>
+          ))}
+        </div>
+
+
     </div>
   );
 }
